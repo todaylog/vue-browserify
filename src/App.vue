17a8764702ad6e2e01770a7a901d14487be02a79
todayLog <bird_@naver.com>
2020-08-06 10:51:12 +0900

    Fix npm script

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,54 @@
 <template>
   <div id="app">
-    <img src="http://vuejs.org/images/logo.png">
+    <img src="http://vuejs.org/images/logo.png" />
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <li>
+        <a href="https://vuejs.org" target="_blank">Core Docs</a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank">Forum</a>
+      </li>
+      <li>
+        <a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a>
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
+      </li>
     </ul>
     <h2>Ecosystem</h2>
     <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vueify" target="_blank">vueify</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li>
+        <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
+      </li>
+      <li>
+        <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
+      </li>
+      <li>
+        <a href="https://github.com/vuejs/vueify" target="_blank">vueify</a>
+      </li>
+      <li>
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+      msg: "Welcome to Your Vue.js App",
+    };
+  },
+};
 </script>
 
 <style scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -45,7 +61,8 @@ img {
   height: 200px;
 }
 
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
